6d8c4ecc17badf58bacfb1b9581cbe87ab8879ec
Gilles Doge <gilles.doge@gmail.com>
2012-09-12 22:38:40 +0200

    secure index_dev.php

--- index_dev.php
+++ index_dev.php
@@ -1,5 +1,17 @@
 <?php
 
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array(
+        '127.0.0.1',
+        '::1',
+        '194.230.78.194',
+    ))
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
 use Symfony\Component\ClassLoader\DebugClassLoader;
 use Symfony\Component\HttpKernel\Debug\ErrorHandler;
 use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
