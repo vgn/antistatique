2b7e93304ec65b3c0faff0f050760430d41f5ff2
Marc Friederich <marc@antistatique.net>
2012-09-14 15:19:00 +0200

    menu blog

--- blog/wp-content/themes/2012/aside-navigation.php
+++ blog/wp-content/themes/2012/aside-navigation.php
@@ -1,16 +1,16 @@
 <nav>
     <ul>
         <li>
-            <a href="<?php echo esc_url( home_url() ); ?>/../">Home</a>
+            <a href="<?php echo esc_url( home_url() ); ?>/../">Accueil</a>
         </li>
         <li>
-            <a href="<?php echo esc_url( home_url() ); ?>/../en/team">Team</a>
+            <a href="<?php echo esc_url( home_url() ); ?>/../fr/team">L'Équipe</a>
         </li>
         <li class="active">
             <a href="/blog">Blog</a>
         </li>
         <li>
-            <a href="<?php echo esc_url( home_url() ); ?>/../en/contact">Contact</a>
+            <a href="<?php echo esc_url( home_url() ); ?>/../fr/contact">Contact</a>
         </li>
     </ul>
 </nav>
