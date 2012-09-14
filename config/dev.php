e26e1c39022d4a83af9f9550637d4974bd20c581
Gilles Doge <gilles@antistatique.net>
2012-09-14 11:29:08 +0200

    set twitter config in the prod env (and not only in the dev env) #13

--- config/dev.php
+++ config/dev.php
@@ -12,10 +12,4 @@ $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../antistatique.log',
 ));
 
-$app['twitter'] = array(
-    'username' => 'antistatique',
-    'hashtag' => '',
-    'api_key' => 'QONcBRXXJBw8o3uzK2atQ' // https://dev.twitter.com/apps/3088947 (logged as antistatique)
-);
-
 $app['analytics'] = null;
--- config/prod.php
+++ config/prod.php
@@ -1,6 +1,13 @@
 <?php
 
 // configure your app for the production environment
+
+$app['twitter'] = array(
+    'username' => 'antistatique',
+    'hashtag' => '',
+    'api_key' => 'QONcBRXXJBw8o3uzK2atQ' // https://dev.twitter.com/apps/3088947 (logged as antistatique)
+);
+
 $app['analytics'] = array(
     'account' => 'UA-4367884-3',
 );
